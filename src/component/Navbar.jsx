9f7132b8300a9f5954b45a1c3d32d93e62ccf9ef
Mubarak <adebowaleraji0103@gmail.com>
2025-07-24 13:03:53 +0100

    d

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,24 @@
-function Navbar () {
-    return(
-        <header>
-            
-        </header>
-    )
+function Navbar() {
+  return (
+    <header>
+      <div>
+        <img src="images/adidas.png" alt="" />
+        <h2>Smart shoe</h2>
+      </div>
+
+      <nav>
+        <ul>
+          <li>HOME</li>
+          <li>SHOP</li>
+          <li>ABOUT</li>
+          <li>COLLECTION</li>
+        </ul>
+
+        <div>
+          <img src="images/cart.png" alt="" />
+        </div>
+      </nav>
+    </header>
+  );
 }
 export default Navbar;
