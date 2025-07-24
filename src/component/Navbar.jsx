d6f313915bc92ae6c27568055fc05fd0ae57ffe1
Mubarak <adebowaleraji0103@gmail.com>
2025-07-24 13:35:50 +0100

    d

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,16 +1,16 @@
 function Navbar() {
   return (
-    <header className="absolute flex justify-between w-full">
+    <header className="absolute flex justify-between w-full text-white font-bold text-[18px]">
       <div className="flex gap-3">
         <img src="images/adidas.png" alt="" />
         <h2>Smart shoe</h2>
       </div>
 
-      <nav className="flex gap-10">
-        <ol className="flex flex-col gap-1">
-          <span className="w-[30px] h-[4px] block bg-black"></span>
-          <span className="w-[30px] h-[4px] block bg-black"></span>
-          <span className="w-[30px] h-[4px] block bg-black"></span>
+      <nav className="flex gap-20 ">
+        <ol className="flex flex-col gap-1 md:hidden">
+          <span className="w-[30px] h-[4px] block bg-white"></span>
+          <span className="w-[30px] h-[4px] block bg-white"></span>
+          <span className="w-[30px] h-[4px] block bg-white"></span>
         </ol>
 
         <ul className="hidden md:flex gap-4">
@@ -20,10 +20,10 @@ function Navbar() {
           <li>COLLECTION</li>
         </ul>
 
+      </nav>
         <div>
           <img src="images/cart.png" alt="" />
         </div>
-      </nav>
     </header>
   );
 }
