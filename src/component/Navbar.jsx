b222b3b567b96b9694e3fcb5647c7193123de3f3
Mubarak <adebowaleraji0103@gmail.com>
2025-07-24 13:41:56 +0100

    f

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,29 +1,31 @@
 function Navbar() {
   return (
-    <header className="absolute flex justify-between w-full text-white font-bold text-[18px]">
+    <header className="absolute flex justify-between px-[1rem] w-full text-white font-bold text-[18px] py-4 md:px-[6rem]">
       <div className="flex gap-3">
         <img src="images/adidas.png" alt="" />
         <h2>Smart shoe</h2>
       </div>
 
-      <nav className="flex gap-20 ">
+      <nav className="flex gap-10 flex-row-reverse md:gap-[10rem] ">
         <ol className="flex flex-col gap-1 md:hidden">
           <span className="w-[30px] h-[4px] block bg-white"></span>
           <span className="w-[30px] h-[4px] block bg-white"></span>
           <span className="w-[30px] h-[4px] block bg-white"></span>
         </ol>
 
-        <ul className="hidden md:flex gap-4">
-          <li>HOME</li>
-          <li>SHOP</li>
-          <li>ABOUT</li>
-          <li>COLLECTION</li>
-        </ul>
-
-      </nav>
         <div>
-          <img src="images/cart.png" alt="" />
+          <ul className="hidden md:flex gap-4">
+            <li>HOME</li>
+            <li>SHOP</li>
+            <li>ABOUT</li>
+            <li>COLLECTION</li>
+          </ul>
+
+          <div>
+            <img src="images/cart.png" alt="" />
+          </div>
         </div>
+      </nav>
     </header>
   );
 }
