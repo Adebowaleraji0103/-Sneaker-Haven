5df75e2052bd7a29bda4ff9134e71f9bcfc562f8
Mubarak <adebowaleraji0103@gmail.com>
2025-07-25 15:31:35 +0100

    d

--- src/component/NewArrival.jsx
+++ src/component/NewArrival.jsx
@@ -1,6 +1,6 @@
 function NewArrival() {
   return (
-    <div className="mb-70 flex flex-col gap[2rem] px-[5px] md:px-[6rem]">
+    <div className="mb-[7rem] flex flex-col gap[2rem] px-[5px] md:px-[6rem]">
       <h1 className="text-[40px] font-bold text-center mb-[2rem] md:text-[49px] md:text-left">New Arrival</h1>
       <div className="w-full flex flex-col gap-[1rem] md:gap-[3rem] md:flex md:flex-row">
         <img src="images/newarrival (3).png" alt="" className="w-full" />
