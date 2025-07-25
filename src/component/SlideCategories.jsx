469fc285990a4d6ec232d441b19b405265d07ee1
Mubarak <adebowaleraji0103@gmail.com>
2025-07-25 09:09:33 +0100

    d

--- src/component/SlideCategories.jsx
+++ src/component/SlideCategories.jsx
@@ -1,14 +1,20 @@
 import { SwiperSlide } from "swiper/react";
 import { Swiper } from "swiper/react";
 
-function SlideCategories () {
-    return(
-        <div>
-            <Swiper>
-                <SwiperSlide></SwiperSlide>
-            </Swiper>
-
-        </div>
-    )
+function SlideCategories() {
+  return (
+    <div>
+      <Swiper>
+        <SwiperSlide><img src="image/slider (1).png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image/slider (8).png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image/slider (7).png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image/slider (6).png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image/slider (5).png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image/slider (4).png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image/slider (3).png" alt="" /></SwiperSlide>
+        <SwiperSlide><img src="image/slider (2).png" alt="" /></SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
 export default SlideCategories;
