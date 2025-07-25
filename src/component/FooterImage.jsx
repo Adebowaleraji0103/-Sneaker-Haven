350b72f00b41b5826803f53999be494e24800424
Mubarak <adebowaleraji0103@gmail.com>
2025-07-25 17:48:37 +0100

    d

--- src/component/FooterImage.jsx
+++ src/component/FooterImage.jsx
@@ -3,8 +3,8 @@ function FooterImage() {
     <div className="mb-70 relative overflow-hidden" >
         <img src="images/a.png" alt="footerimage" className="w-full "/>
 
-      <div className="flex flex-col gap-5 absolute top-0 inset-0 text-white font-bold text-center items-center justify-center ">
-        <h1 className="text-[40px]">Get discount for membership</h1>
+      <div className="flex flex-col gap-5 absolute top-0 inset-0 text-white text-center items-center justify-center ">
+        <h1 className="text-[40px] leading-10 font-bold">Get discount for membership</h1>
         <p>Every membership will get a 40% discount for the first purchase</p>
         <button>Join now</button>
       </div>
