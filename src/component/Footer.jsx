2004580e54fc729822b44204d8bac2201145dd31
Mubarak <adebowaleraji0103@gmail.com>
2025-07-25 18:44:58 +0100

    d

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,10 +1,10 @@
 function Footer() {
   return (
-    <div className="mb-70 flex flex-col justify-between text-center   md: md:flex-row">
+    <div className="flex flex-col text-center pb-20 md:justify-between md:px-[8rem] md: md:flex-row">
       <div className="flex flex-col gap-5 md:flex-row md:gap-[7rem]">
         <div className="flex flex-col gap-3">
           <h1 className="text-[22px] font-bold ">Shops</h1>
-          <ul>
+          <ul className="flex flex-col gap-2 text-[14px] font-semibold">
             <li>Men</li>
             <li>Women</li>
             <li>Kids</li>
@@ -12,8 +12,8 @@ function Footer() {
         </div>
 
         <div className="flex flex-col gap-3">
-          <h1 className="text-[22px] font-bold ">Connect</h1>
-          <ul>
+          <h1 className="text-[22px] font-bold">Connect</h1>
+          <ul className="flex flex-col gap-2 text-[14px] font-semibold">
             <li>LinkedIn</li>
             <li>Facebook</li>
             <li>Twitter</li>
@@ -23,15 +23,15 @@ function Footer() {
 
         <div className="flex flex-col gap-3">
           <h1 className="text-[22px] font-bold ">Connect</h1>
-          <p>hello.bright@gmail.com</p>
+          <p className="flex flex-col gap-2 text-[14px] font-semibold">hello.bright@gmail.com</p>
           <img src="images/adidas.png" alt="" />
         </div>
       </div>
 
-      <div className="flex flex-col gap-3">
+      <div className="flex flex-col gap-3 items-center">
         <h1 className="text-[22px] font-bold ">Stay in touch</h1>
-        <div>
-          <p> Email address </p>
+        <div className="flex justify-center gap-[3rem] items-center">
+          <p className="flex flex-col gap-2 text-[14px] font-semibold"> Email address </p>
           <img src="images/Vector.png" alt="" />
         </div>
         <span className="bg-black h-[1px] inline-block w-[200px]"></span>
