fd1b8b6bccac52bc9b79b2940df459f0fe2c7cb8
Mubarak <adebowaleraji0103@gmail.com>
2025-07-25 08:25:28 +0100

    s

--- src/component/Cartegories.jsx
+++ src/component/Cartegories.jsx
@@ -1,7 +1,7 @@
 function Cartigories () {
     return(
-        <div className="mb-70 ">
-            <h1 className="text-[40px] font-bold text-center md:text-[49px] md:text-right">Categories</h1>
+        <div className="mb-70 px-[2rem]">
+            <h1 className="text-[40px] font-bold text-center md:text-[49px] md:text-left">Categories</h1>
         </div>
     )
 }
