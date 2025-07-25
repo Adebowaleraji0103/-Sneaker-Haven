55969b4488a001b26fa35d959b6e0e64ff08c133
Mubarak <adebowaleraji0103@gmail.com>
2025-07-25 18:11:20 +0100

    d

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -2,21 +2,38 @@ function Footer() {
   return (
     <div className="mb-70">
       <div>
-        <h1>Shops</h1>
-        <ul>
-          <li>Men</li>
-          <li>Women</li>
-          <li>Kids</li>
-        </ul>
+        <div>
+          <h1>Shops</h1>
+          <ul>
+            <li>Men</li>
+            <li>Women</li>
+            <li>Kids</li>
+          </ul>
+        </div>
+
+        <div>
+          <h1>Connect</h1>
+          <ul>
+            <li>LinkedIn</li>
+            <li>Facebook</li>
+            <li>Twitter</li>
+            <li>Instagram</li>
+          </ul>
+        </div>
+
+        <div>
+          <h1>Connect</h1>
+          <li>hello.bright@gmail.com</li>
+          <img src="images/adidas.png" alt="" />
+        </div>
       </div>
 
       <div>
-        <h1>Shops</h1>
-        <ul>
-          <li>Men</li>
-          <li>Women</li>
-          <li>Kids</li>
-        </ul>
+        <h1>Stay in touch</h1>
+
+        <div>
+            
+        </div>
       </div>
     </div>
   );
