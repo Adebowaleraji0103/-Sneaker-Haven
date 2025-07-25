811215953e1a39b178d78e42704a2685df5aca6d
Mubarak <adebowaleraji0103@gmail.com>
2025-07-25 15:52:50 +0100

    d

--- src/component/FooterImage.jsx
+++ src/component/FooterImage.jsx
@@ -1,7 +1,7 @@
 function FooterImage() {
   return (
     <div className="mb-70 relative overflow-hidden">
-        <img src="images/footerimage.png" alt="footerImage" className="h-[40rem] object-cover"/>
+        <img src="images/footerimage.jpg" alt="footerimage" className="w-full"/>
 
       <div className="flex flex-col gap-5 absolute top-0 text-white font-bold items-center justify-center ">
         <h1>Get discount for membership</h1>
