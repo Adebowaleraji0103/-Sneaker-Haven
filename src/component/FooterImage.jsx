1dd8a49805de32e413550dbc4136b883742a22d9
Mubarak <adebowaleraji0103@gmail.com>
2025-07-25 15:40:47 +0100

    s

--- src/component/FooterImage.jsx
+++ src/component/FooterImage.jsx
@@ -1,8 +1,15 @@
-function FooterImage () {
-    return(
-        <div className="mb-70">
-            <img src="images/footerimage.png" alt="footerImage" />
-        </div>
-    )
+function FooterImage() {
+  return (
+    <div className="mb-70">
+      <div className="relative">
+        <img src="images/footerimage.png" alt="footerImage" className="relative" />
+      </div>
+      <div className="flex flex-col gap-5 absolute items-center">
+        <h1>Get discount for membership</h1>
+        <p>Every membership will get a 40% discount for the first purchase</p>
+        <button>Join now</button>
+      </div>
+    </div>
+  );
 }
 export default FooterImage;
