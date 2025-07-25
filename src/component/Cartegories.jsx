8ac9a8f1ec8114415cf7028783fd30e55891e7ec
Mubarak <adebowaleraji0103@gmail.com>
2025-07-25 08:37:13 +0100

    d

--- src/component/Cartegories.jsx
+++ src/component/Cartegories.jsx
@@ -1,8 +1,16 @@
-function Cartigories () {
-    return(
-        <div className="mb-70 px-[2rem] md:px-[5rem]">
-            <h1 className="text-[40px] font-bold text-center md:text-[49px] md:text-left">Categories</h1>
-        </div>
-    )
+function Cartigories() {
+  return (
+    <div className="mb-70 px-[2rem] md:px-[5rem]">
+      <h1 className="text-[40px] font-bold text-center md:text-[49px] md:text-left">
+        Categories
+      </h1>
+
+      <div className="md:flex justify-between">
+        <img src="images/categories (3).png" alt="" />
+        <img src="images/categories (2).png" alt="" />
+        <img src="images/categories (1).png" alt="" />
+      </div>
+    </div>
+  );
 }
 export default Cartigories;
