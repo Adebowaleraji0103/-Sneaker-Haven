ae94f3501c55537333894191c10d4393dc584384
Mubarak <adebowaleraji0103@gmail.com>
2025-08-05 12:09:39 +0100

    d

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,11 @@
 import { LuMenu } from "react-icons/lu";
+import { useState } from "react";
 
 function Navbar() {
+
+  const [menuIsOpen, setMenuIsOpen] = useState (false)
   return (
-    <header className="absolute flex justify-between items-center px-[1rem] py-6 w-full text-white font-semibold text-[16px] md:px-[6rem]">
+    <header className="relative flex justify-between items-center px-[1rem] py-6 w-full text-white font-semibold text-[16px] md:px-[6rem]">
       <div className="flex gap-2 md:gap-3 ">
         <img src="images/adidas.png" alt=""/>
         <h2 className="text-[18px] md:text-[25px]">Smart shoe</h2>
@@ -14,7 +17,7 @@ function Navbar() {
         </div>
 
         <ol className="flex flex-col gap-1 md:hidden">
-          <LuMenu />
+          <LuMenu onClick={()=> setMenuIsOpen (!menuIsOpen)}/>
         </ol>
 
         <ul className="hidden md:flex gap-10 ">
@@ -24,6 +27,14 @@ function Navbar() {
           <li>COLLECTION</li>
         </ul>
       </nav>
+      {menuIsOpen && (
+        <ul className="absolute top-0 right-0  bg-amber-500">
+          <li>HOME</li>
+          <li>SHOP</li>
+          <li>ABOUT</li>
+          <li>COLLECTION</li>
+        </ul>
+      )}
     </header>
   );
 }
