9f56ab3f39dfffaf20faf4c00a492efa330a2d6e
Mubarak <adebowaleraji0103@gmail.com>
2025-08-06 12:09:52 +0100

    d

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -48,19 +48,18 @@ function Navbar() {
         </ul>
       </nav>
       {menuIsOpen && (
-        <div className="flex justify-between flex-row-reverse gap-[1.5rem] text-[14px] py-[2rem] px-[1rem] absolute top-0 left-0 w-[50%] h-[30rem] bg-[#04251C] md:hidden">
+        <div className="text-[14px] py-[2rem] px-[1rem] absolute top-0 left-0 w-[50%] h-[30rem] bg-[#04251C] md:hidden">
           <IoMdClose
             onClick={() => setMenuIsOpen(false)}
             className="text-[24px] transition-all duration-300"
           />
 
           <ul className="flex flex-col gap-[2rem]">
-            <a href="#banner">
-              <li className="hover:text-[lightgrey] cursor-pointer">HOME</li>
-            </a>
-            <li>SHOP</li>
-            <li>ABOUT</li>
-            <li>COLLECTION</li>
+            {navLinks.map((link, index) => (
+              <li key={index} className="hover:text-[lightgrey] cursor-pointer">
+                <a href={link.href}>{link.label}</a>
+              </li>
+            ))}
           </ul>
         </div>
       )}
