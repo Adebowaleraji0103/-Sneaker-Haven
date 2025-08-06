091fa7f6711a4317a3e5bf93410a032159725102
Mubarak <adebowaleraji0103@gmail.com>
2025-08-06 18:54:57 +0100

    d

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -45,8 +45,7 @@ function Navbar() {
         <div className="flex flex-row-reverse justify-between text-[14px] py-[2rem] px-[1rem] absolute top-0 left-0 w-[50%] h-[30rem] bg-[#04251C] md:hidden">
           <IoMdClose
             onClick={() => setMenuIsOpen(false)}
-            className="text-[24px] transition-all duration-300"
-          />
+            className="text-[24px] transition-all duration-300 fixed" />
 
           <ul className="flex flex-col gap-[2rem]">
             {navLinks.map((link, index) => (
